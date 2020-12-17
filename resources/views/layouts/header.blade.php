7b9213b50bd619505ce108dbc754e6d588834810
Alexander Briones <alexanderbvart@gmail.com>
2020-12-17 16:45:41 -0500

    apache + prod + seed usuarios -admin

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,6 +8,17 @@
             @if (Route::has('login'))
                 @auth
                     <a class="mr-5 hover:text-gray-900" href="{{ url('/home') }}">Home</a>
+                    <span>{{ Auth::user()->name }}</span>
+                    <a 
+                                class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" 
+                                href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                              document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 @else
                     <a class="mr-5 hover:text-gray-900" href="{{ route('login') }}">Login</a>
 
